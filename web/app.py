965a09200374fb5e5648efa97bb41653af7d1910
Revant Tiwari <tiwrevan@amazon.com>
2023-06-09 14:50:24 +0530

    adding CORS

--- web/app.py
+++ web/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request
 from flask_restful import reqparse, abort, Api, Resource
 from main import return_ticket
+from GetContainer import returnSelfServeStatus
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 parser = reqparse.RequestParser()
@@ -12,6 +16,11 @@ class Find_Team(Resource):
         # abort_if_todo_doesnt_exist(question)
         print(request.form["message"])
         return {"Response":return_ticket(request.form["message"])[0]}
+
+class CheckSelfServe(Resource):
+    def post(self):
+        return {"Response": returnSelfServeStatus(request.form["message"],True)}
+
 
 api.add_resource(Find_Team, '/Find_Team/')
 
