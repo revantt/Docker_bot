9d2e9234b44199803417e4763ce36a85dc351801
Revant Tiwari <tiwrevan@amazon.com>
2023-06-11 18:45:34 +0530

    More changes

--- web/app.py
+++ web/app.py
@@ -13,17 +13,35 @@ api = Api(app)
 parser = reqparse.RequestParser()
 
 def cut_ticket(payload):
-    s = """  aws tickety --region us-west-2 --endpoint-url https://us-west-2.api.tickety.amazon.dev create-ticket --ticket '{"title": "test ticket", "severity": "SEV_5", "description":"description","categorization": [{"key": "category", "value":"Ticketing"}, {"key": "type", "value":"Wonka"}, {"key": "item","value":"Integration Tests"} ] }' --aws-account-id 474955757919 --ticketing-system-name Default"""
-    proc = subprocess.Popen([s], stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    out = out.decode("utf-8")
+    description = payload["desc"]
+    impact = payload["impactedShipment"]
+    shipment = payload["shipmentsExample"]
+    # s = """  aws tickety --region us-west-2 --endpoint-url https://us-west-2.api.tickety.amazon.dev create-ticket --ticket '{"title": "test ticket", "severity": "SEV_5", "description":"description","categorization": [{"key": "category", "value":"Ticketing"}, {"key": "type", "value":"Wonka"}, {"key": "item","value":"Integration Tests"} ] }' --aws-account-id 474955757919 --ticketing-system-name Default"""
+    final_desc = "Description of issue " + "\n" + description + "\n" + "Impact of the severity" + impact + "\n" + "Shipment(s) impacted \n " + " \n ".join(shipment)
+    s = """  aws tickety --region us-west-2 --endpoint-url https://us-west-2.api.tickety.amazon.dev create-ticket --ticket '{"title": "test ticket", "severity": "SEV_5", "description":""" + final_desc + ""","categorization": [{"key": "category", "value":"Ticketing"}, {"key": "type", "value":"Wonka"}, {"key": "item","value":"Integration Tests"} ] }' --aws-account-id 474955757919 --ticketing-system-name Default"""
+    # proc = subprocess.Popen([s], stdout=subprocess.PIPE, shell=True)
+    # (out, err) = proc.communicate()
+    # out = out.decode("utf-8")
+    out = s
+    return out
 
 
 class Find_Team(Resource):
     def post(self):
         # abort_if_todo_doesnt_exist(question)
-        print(request.get_json()["message"])
-        return {"Response":return_ticket(request.get_json()["message"])[0]}
+        print(request.get_json()["desc"])
+        payload = request.get_json()
+        print(payload["shipmentsExample"])
+        try:
+            result = {"Response": return_ticket(payload["desc"])[0],
+                  "AWS_response" : cut_ticket(payload),
+                    "SelfServeStatus" : returnSelfServeStatus(payload["shipmentsExample"][0],True)
+                  }
+            return result
+        except Excpetion as e:
+            return {"Response": "Some error occured " + e}
+        # return {"Response":return_ticket(request.get_json()["desc"])[0]}
+
 
 
 api.add_resource(Find_Team, '/Find_Team/')
