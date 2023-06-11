6325ce77d58fdd582dee15e5031e3d4d3314d753
Revant Tiwari <tiwrevan@amazon.com>
2023-06-11 17:36:35 +0530

    Adding ticket cutting

--- web/app.py
+++ web/app.py
@@ -3,6 +3,7 @@ from flask_restful import reqparse, abort, Api, Resource
 from main import return_ticket
 from GetContainer import returnSelfServeStatus
 from flask_cors import CORS
+import subprocess
 
 
 app = Flask(__name__)
@@ -10,6 +11,13 @@ CORS(app)
 api = Api(app)
 
 parser = reqparse.RequestParser()
+
+def cut_ticket(payload):
+    s = """  aws tickety --region us-west-2 --endpoint-url https://us-west-2.api.tickety.amazon.dev create-ticket --ticket '{"title": "test ticket", "severity": "SEV_5", "description":"description","categorization": [{"key": "category", "value":"Ticketing"}, {"key": "type", "value":"Wonka"}, {"key": "item","value":"Integration Tests"} ] }' --aws-account-id 474955757919 --ticketing-system-name Default"""
+    proc = subprocess.Popen([s], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    out = out.decode("utf-8")
+
 
 class Find_Team(Resource):
     def post(self):
